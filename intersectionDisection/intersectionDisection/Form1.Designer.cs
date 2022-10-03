36358c616788e604c10d237b2795dddfadc1cca6
Jeroen van Dam <jeroen.vandam@outlook.com>
2022-10-03 11:39:39 +0200

    progress op visuals intersections

--- intersectionDisection/intersectionDisection/Form1.Designer.cs
+++ intersectionDisection/intersectionDisection/Form1.Designer.cs
@@ -51,19 +51,19 @@ namespace intersectionDisection
             this.Text = "Intersection Disection";
 
 
-
             switch (whatIntersection)
             {
                 case "fourwayIntersection":
                     this.fourwayIntersection();
-                    this.Paint += this.Teken;
+                    this.Paint += this.TekenFourWay;
                     break;
                 case "fourwayWithLeftLane":
                     this.fourwayWithLeftLane();
+                    this.Paint += this.TekenFourWayWithLeftLane;
                     break;
                 default:
                     this.fourwayIntersection();
-                    this.Paint += this.Teken;
+                    this.Paint += this.TekenFourWay;
                     break;
             }
 
@@ -173,7 +173,100 @@ namespace intersectionDisection
 
         private void fourwayWithLeftLane()
         {
-            throw new NotImplementedException();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelnorth = new System.Windows.Forms.Label();
+            this.labeleast = new System.Windows.Forms.Label();
+            this.labelsouth = new System.Windows.Forms.Label();
+            this.labelwest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "North: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(985, 450);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "East: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 950);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "South: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 550);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "West: ";
+            // 
+            // labelnorth
+            // 
+            this.labelnorth.AutoSize = true;
+            this.labelnorth.Location = new System.Drawing.Point(400, 50);
+            this.labelnorth.Name = "labelnorth";
+            this.labelnorth.Size = new System.Drawing.Size(35, 13);
+            this.labelnorth.TabIndex = 4;
+            this.labelnorth.Text = "North: ";
+            // 
+            // labeleast
+            // 
+            this.labeleast.AutoSize = true;
+            this.labeleast.Location = new System.Drawing.Point(950, 450);
+            this.labeleast.Name = "labeleast";
+            this.labeleast.Size = new System.Drawing.Size(35, 13);
+            this.labeleast.TabIndex = 5;
+            this.labeleast.Text = "East: ";
+            // 
+            // labelsouth
+            // 
+            this.labelsouth.AutoSize = true;
+            this.labelsouth.Location = new System.Drawing.Point(500, 950);
+            this.labelsouth.Name = "labelsouth";
+            this.labelsouth.Size = new System.Drawing.Size(35, 13);
+            this.labelsouth.TabIndex = 6;
+            this.labelsouth.Text = "South: ";
+            // 
+            // labelwest
+            // 
+            this.labelwest.AutoSize = true;
+            this.labelwest.Location = new System.Drawing.Point(50, 550);
+            this.labelwest.Name = "labelwest";
+            this.labelwest.Size = new System.Drawing.Size(35, 13);
+            this.labelwest.TabIndex = 7;
+            this.labelwest.Text = "West: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelnorth);
+            this.Controls.Add(this.labeleast);
+            this.Controls.Add(this.labelsouth);
+            this.Controls.Add(this.labelwest);
         }
         #endregion
     }
--- intersectionDisection/intersectionDisection/Form1.cs
+++ intersectionDisection/intersectionDisection/Form1.cs
@@ -15,7 +15,7 @@ namespace intersectionDisection
     {
         public Intersection intersection;
         public TrafficLights trafficLights;
-        private string whatInt = "fourwayIntersection";
+        private string whatInt = "fourwayIntersection"; // fourwayWithLeftLane, fourwayIntersection
 
         public delegate void delUpdateTextBox(string text);
 
@@ -28,6 +28,7 @@ namespace intersectionDisection
             this.intersection = new Intersection(new int[] { 2, 3, 4, 1 }, 10, this.trafficLights);
             this.trafficLights.intersection = this.intersection;
             this.InitializeComponent(whatInt);
+
             threadStart = new ThreadStart(StartSimulation);
             updateThread = new Thread(threadStart);
             updateThread.Start();
@@ -83,11 +84,14 @@ namespace intersectionDisection
             this.label4.Text = str[3];
         }
 
-        void Teken(object obj, PaintEventArgs pea)
+        void TekenFourWay(object obj, PaintEventArgs pea)
         {
             Graphics gr = pea.Graphics;
             Brush hor = Brushes.Black;
             Brush ver = Brushes.Black;
+
+            Pen smalPen = new Pen(Color.Black, 1);
+            Pen bigPen = new Pen(Color.Black, 2);
 
 
             if (this.intersection.HorizontalLight)
@@ -101,23 +105,82 @@ namespace intersectionDisection
                 ver = Brushes.Green;
             }
 
-            gr.DrawLine(Pens.Black, 400, 100, 400, 400); //   |
-            gr.DrawLine(Pens.Black, 100, 400, 400, 400); // --
+            gr.DrawLine(bigPen, 200, 50, 200, 200); //   |
+            gr.DrawLine(bigPen, 50, 200, 200, 200); // --
+            gr.DrawLine(smalPen, 50, 250, 200, 250);
 
-            gr.DrawLine(Pens.Black, 600, 100, 600, 400); //     |
-            gr.DrawLine(Pens.Black, 600, 400, 900, 400); //      --
+            gr.DrawLine(bigPen, 300, 50, 300, 200); //     |
+            gr.DrawLine(bigPen, 300, 200, 450, 200); //      --
+            gr.DrawLine(smalPen, 250, 50, 250, 200);
 
-            gr.DrawLine(Pens.Black, 400, 600, 400, 900); //   |
-            gr.DrawLine(Pens.Black, 100, 600, 400, 600); // --
+            gr.DrawLine(bigPen, 200, 300, 200, 450); // --
+            gr.DrawLine(bigPen, 50, 300, 200, 300); //   |
+            gr.DrawLine(smalPen, 250, 300, 250, 450);
 
-            gr.DrawLine(Pens.Black, 600, 600, 600, 900); //     |
-            gr.DrawLine(Pens.Black, 600, 600, 900, 600); //      --
+            gr.DrawLine(bigPen, 300, 300, 300, 450); //    --
+            gr.DrawLine(bigPen, 300, 300, 450, 300); //   |  
+            gr.DrawLine(smalPen, 300, 250, 450, 250);
 
-            gr.FillEllipse(hor, 425, 325, 50, 50);
-            gr.FillEllipse(hor, 525, 625, 50, 50);
 
-            gr.FillEllipse(ver, 325, 525, 50, 50);
-            gr.FillEllipse(ver, 625, 425, 50, 50);
+            gr.FillEllipse(hor, 180, 270, 20, 20);
+            gr.FillEllipse(hor, 300, 210, 20, 20);
+
+            gr.FillEllipse(ver, 210, 180, 20, 20);
+            gr.FillEllipse(ver, 270, 300, 20, 20);
+        }
+
+        void TekenFourWayWithLeftLane(object obj, PaintEventArgs pea)
+        {
+            Graphics gr = pea.Graphics;
+            Brush hor = Brushes.Black;
+            Brush ver = Brushes.Black;
+            Pen dashPen = new Pen(Color.Black, 1) { DashPattern = new[] { 5f, 5f } };
+            Pen smalPen = new Pen(Color.Black, 1);
+            Pen bigPen = new Pen(Color.Black, 2);
+
+
+            if (this.intersection.HorizontalLight)
+            {
+                hor = Brushes.Green;
+                ver = Brushes.Red;
+            }
+            else
+            {
+                hor = Brushes.Red;
+                ver = Brushes.Green;
+            }
+
+            gr.DrawLine(bigPen, 200, 50, 200, 200); //   |
+            gr.DrawLine(bigPen, 50, 200, 200, 200); // --
+            gr.DrawLine(smalPen, 50, 250, 200, 250);
+            gr.DrawLine(dashPen, 125, 300, 200, 300);
+
+            gr.DrawLine(bigPen, 350, 50, 350, 200); //     |
+            gr.DrawLine(bigPen, 350, 200, 500, 200); //      --
+            gr.DrawLine(smalPen, 300, 50, 300, 200);
+            gr.DrawLine(dashPen, 250, 125, 250, 200);
+
+            gr.DrawLine(bigPen, 200, 350, 200, 500); // --
+            gr.DrawLine(bigPen, 50, 350, 200, 350); //   |
+            gr.DrawLine(smalPen, 250, 350, 250, 500);
+            gr.DrawLine(dashPen, 300, 350, 300, 425);
+
+            gr.DrawLine(bigPen, 350, 350, 350, 500); //    --
+            gr.DrawLine(bigPen, 350, 350, 500, 350); //   |  
+            gr.DrawLine(smalPen, 350, 300, 500, 300);
+            gr.DrawLine(dashPen, 350, 250, 425, 250);
+
+            gr.FillEllipse(hor, 180, 320, 20, 20);
+            gr.FillEllipse(hor, 300, 210, 20, 20);
+            gr.FillEllipse(hor, 300, 210, 20, 20);
+            gr.FillEllipse(hor, 300, 210, 20, 20);
+
+
+            gr.FillEllipse(ver, 210, 180, 20, 20);
+            gr.FillEllipse(ver, 320, 300, 20, 20);
+            gr.FillEllipse(ver, 320, 300, 20, 20);
+            gr.FillEllipse(ver, 320, 300, 20, 20);
+
         }
 
     }
